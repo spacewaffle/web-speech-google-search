2019c89229355b4632ac3eb57f8a6db1fe5cf64e
Jon Cheng <jon.syndicate@gmail.com>
2013-06-11 15:56:36 -0400

    setup background event listeners and messaging

--- public/extension/background.js
+++ public/extension/background.js
@@ -1,5 +1,20 @@
 //setup event listeners for tab switching
 
 //on current tab switch, do the following
-//set recognition abort for all other tabs
-//set recognition start for current tab
+chrome.tabs.onActivated.addListener(function(info) {
+  //set recognition abort for all other tabs
+  //loop through all tabs and send an abort message
+  chrome.tabs.query({'windowId': windowID}, function(tab){
+    chrome.tabs.sendMessage(tab.id, {greeting: "abort"}, function(response) {
+      console.log(response.farewell);
+    });
+  });
+
+  //set recognition start for current tab
+  chrome.tabs.get(info.tabId, function(tab) {
+    //send this tab a message to send a start message
+    chrome.tabs.sendMessage(tab.id, {greeting: "start"}, function(response) {
+      console.log(response.farewell);
+    });
+  });
+});
