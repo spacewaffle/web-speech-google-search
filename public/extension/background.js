3785f737766d1e66d0b37b5cb93d7bb58b5ad0cd
Jon Cheng <jon.syndicate@gmail.com>
2013-06-12 03:03:06 -0400

    removed some unnecessary console logging

--- public/extension/background.js
+++ public/extension/background.js
@@ -1,24 +1,20 @@
 //setup event listeners for tab switching
 console.log('running background.js');
 //on current tab switch, do the following
-console.log(chrome.tabs);
 chrome.tabs.onActivated.addListener(function(info) {
   console.log('a tab has been activated');
   //set recognition abort for all other tabs
-  console.log(chrome.tabs);
   //loop through all tabs and send an abort message
   console.log("activated window's id is" + info.windowId);
   chrome.tabs.query({windowId: info.windowId}, function(tabs){
-  console.log(chrome.tabs);
     console.log('starting query');
     for (var i = tabs.length - 1; i >= 0; i--) {
-      console.log(tabs[i]);
+      
       console.log('sending abort');
       //eventually should check if tab is the activated tab before aborting
       chrome.tabs.sendMessage(tabs[i].id, {greeting: "abort"}, function(response) {
       });
     }
-    console.log(chrome.tabs);
   });
 
   //set recognition start for current tab
