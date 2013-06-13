8eec7823bb3232cd86dcd57152e68f0d0d8f3461
Jon Cheng <jon.syndicate@gmail.com>
2013-06-12 23:44:54 -0400

    first stab using onupdated listener

--- public/extension/background.js
+++ public/extension/background.js
@@ -2,38 +2,36 @@
 console.log('running background.js');
 
 //when switching tabs, move speech recognition to the active tab
-chrome.tabs.onActivated.addListener(function(info) {
+chrome.tabs.onUpdated.addListener(function(tabId, info, tab) {
   console.log('a tab has been activated');
   //set recognition stop for all other tabs
   //loop through all tabs and send an stop message
-  chrome.tabs.get(info.tabId, function(tab) {
-    console.log('tab url is ' + tab.url);
-    if(tab.url.substring(0,15) != "chrome-devtools"){
-      //stop speech recognition in all tabs
-      chrome.tabs.query({}, function(tabs){
-        console.log('starting query');
-        for (var i = tabs.length - 1; i >= 0; i--) {
-          if(tabs[i].url.substring(0,6) != "chrome"){
-            if(tabs[i].id !=info.tabId){
-              console.log('sending stop to ' + tabs[i].title + " id: " + tabs[i].id);
-              //eventually should check if tab is the activated tab before stoping
-              chrome.tabs.sendMessage(tabs[i].id, {greeting: "stop"}, function(response) {
-              });
-            }
+  console.log('tab url is ' + tab.url);
+  if(tab.url.substring(0,15) != "chrome-devtools"){
+    //stop speech recognition in all tabs
+    chrome.tabs.query({}, function(tabs){
+      console.log('starting query');
+      for (var i = tabs.length - 1; i >= 0; i--) {
+        if(tabs[i].url.substring(0,6) != "chrome"){
+          if(tabs[i].id != tabId){
+            console.log('sending stop to ' + tabs[i].title + " id: " + tabs[i].id);
+            //eventually should check if tab is the activated tab before stoping
+            chrome.tabs.sendMessage(tabs[i].id, {greeting: "stop"}, function(response) {
+            });
           }
         }
+      }
 
-        //set recognition start for current tab
-        console.log('sending start to ' + tab.title + " id: " + tab.id);
-        //console.log('tab is ' + tab);
-        //send this tab a message to send a start message
-        // if(tab.id){
-          chrome.tabs.sendMessage(tab.id, {greeting: "start"}, function(response) {
-          });
-        // }
-      });
-    }
-  });
+      //set recognition start for current tab
+      console.log('sending start to ' + tab.title + " id: " + tab.id);
+      //console.log('tab is ' + tab);
+      //send this tab a message to send a start message
+      // if(tab.id){
+        chrome.tabs.sendMessage(tabId, {greeting: "start"}, function(response) {
+        });
+      // }
+    });
+  }
 });
 
 
