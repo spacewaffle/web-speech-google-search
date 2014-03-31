51e2574671406362b7f8b94de378f0f0439bcd1f
Jon Cheng <jon.syndicate@gmail.com>
2014-03-31 16:54:53 -0400

    updated receiver js and added scroll top functionality

--- receiver.js
+++ receiver.js
@@ -6,13 +6,13 @@ console.log('added receiver');
 chrome.extension.onMessage.addListener( function(request,sender,sendResponse){
   if( request.greeting === "do_action" ){
     var action = request.action;
-    var result = request.result;
+    var modifier = request.modifier;
     var last_action = request.last_action;
-    var last_result = request.last_result;
-    console.log(action + result);
+    var last_modifier = request.last_modifier;
+    console.log(action + modifier);
     switch (action){
       case "search":
-        window.location = search_url+result;
+        window.location = search_url+modifier.replace(" ", "+");
         break;
       case "back":
         //history.back doesn't do a full reload so no js :/
@@ -23,30 +23,35 @@ chrome.extension.onMessage.addListener( function(request,sender,sendResponse){
         history.forward();
         break;
       case "go to":
-        window.location = search_url+result+"&btnI";
+        window.location = search_url+modifier.replace(" ", "+")+"&btnI";
         break;
       case "wiki":
-        window.location = wiki_url+result;
+        window.location = wiki_url+modifier.replace(" ", "+");
         break;
       case "reload":
         window.location = window.location;
         break;
       case "scroll":
         var doc_height = $(document).height();
-        if(result == "down+fast"){
+        if(modifier == "down+fast"){
           $("html, body").animate({
             scrollTop: doc_height
           }, doc_height*3, "linear");
         }
-        else if(result == "up"){
+        else if(modifier == "up"){
           $("html, body").animate({
             scrollTop: 0
           }, doc_height*5, "linear");
         }
-        else if(result == "up+fast"){
+        else if(modifier == "up fast"){
           $("html, body").animate({
             scrollTop: 0
           }, doc_height*3, "linear");
+        }
+        else if(modifier.indexOf("top") >=0 || modifier.indexOf("to top") >=0){
+          $("html, body").animate({
+            scrollTop: 0
+          }, 1000, "linear");
         }
         else{
           $("html, body").animate({
@@ -56,20 +61,20 @@ chrome.extension.onMessage.addListener( function(request,sender,sendResponse){
         break;
       case "stop":
         console.log("last action is " + last_action);
-        console.log("last result is " + last_result);
-        if(last_action == "scroll" && last_result == "down" || last_result == "down+fast"){
+        console.log("last modifier is " + last_modifier);
+        if(last_action == "scroll" && last_modifier == "down" || last_modifier == "down+fast"){
           $("html, body").stop().animate({
             scrollTop: $("body").scrollTop()-200
           }, 1000);
         }
-        else if(last_action == "scroll" && last_result == "up" || last_result == "up+fast"){
+        else if(last_action == "scroll" && last_modifier == "up" || last_modifier == "up+fast"){
           $("html, body").stop().animate({
             scrollTop: $("body").scrollTop()+200
           }, 1000);
         }
         else{
           $("html, body").stop().animate({
-            scrollTop: $("body").scrollTop()
+            scrollTop: $("body").scrollTop()-200
           }, 0);
         }
         break;
