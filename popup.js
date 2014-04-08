46eea83e326538dafea667e17a4c8ee4db436f11
Jon Cheng <jon.syndicate@gmail.com>
2014-04-08 14:01:31 -0400

    create a new options page for settings

--- popup.js
+++ popup.js
@@ -35,14 +35,6 @@ var commands = {
   "click": ["click", "clique", "quick"]
 };
 
-chrome.storage.sync.get('auto_start', function(items) {
-  //check stored settings if we should start on launch
-  if(items["auto_start"]){
-    document.getElementById('auto_start').checked = true;
-  }
-});
-
-
 console.log('loading voice search js');
 function startRecognition(){
   console.log('starting recognition');
@@ -139,9 +131,3 @@ function startRecognition(){
 }
 
 startRecognition();
-
-//whenever the user checks or unchecks auto_start, update their settings
-var el_auto_start = document.getElementById("auto_start");
-el_auto_start.addEventListener("change", function(){
-  chrome.storage.sync.set({'auto_start': el_auto_start.checked});
-});
