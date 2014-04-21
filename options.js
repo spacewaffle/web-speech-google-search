e68f2babae8b312b6062c0dce900a57e5ea521dd
Jon Cheng <jon.syndicate@gmail.com>
2014-04-21 16:36:51 -0400

    prevent creation of custom commands without pro

--- options.js
+++ options.js
@@ -1,5 +1,12 @@
 var storage = chrome.storage.sync;
 
+var pro_license = false;
+storage.get("pro_license", function(items){
+  if(items["pro_license"]){
+    pro_license = true;
+  }
+});
+
 storage.get('auto_start', function(items) {
   //check stored settings if we should start on launch
   if(items["auto_start"] !== undefined){
@@ -105,41 +112,51 @@ update_commands();
 
 var submit_command = document.getElementById("submit_command");
 submit_command.addEventListener("click", function(){
-  var alias = document.getElementById("custom-command").value;
-  var site_to_visit = document.getElementById("site-to-visit").value;
+  if(pro_license){
+    var alias = document.getElementById("custom-command").value;
+    var site_to_visit = document.getElementById("site-to-visit").value;
 
-  console.log("submitted new command");
-  storage.get('custom_commands', function(items){
-    var commands = [];
+    console.log("submitted new command");
+    storage.get('custom_commands', function(items){
+      var commands = [];
 
-    console.log("received all commands");
-    console.log(items);
-    console.log(items["custom_commands"]);
-    //return the custom command array or an empty one
-    if(items["custom_commands"]){
-      commands = items["custom_commands"];
-    }
-    console.log('commands var is');
-    console.log(commands);
-    //appending the new command to the array
+      console.log("received all commands");
+      console.log(items);
+      console.log(items["custom_commands"]);
+      //return the custom command array or an empty one
+      if(items["custom_commands"]){
+        commands = items["custom_commands"];
+      }
+      console.log('commands var is');
+      console.log(commands);
+      //appending the new command to the array
 
-    var new_command = {};
-    new_command[alias] = site_to_visit;
-    commands.push(new_command);
+      var new_command = {};
+      new_command[alias] = site_to_visit;
+      commands.push(new_command);
 
-    console.log("updated commands are ");
-    console.log(commands);
+      console.log("updated commands are ");
+      console.log(commands);
 
-    //storing the new command
-    storage.set({'custom_commands': commands }, function(){
-      update_commands();
+      //storing the new command
+      storage.set({'custom_commands': commands }, function(){
+        update_commands();
+      });
+      console.log('finished setting commands');
+
+      //resetting input fields
+      document.getElementById("custom-command").value = "";
+      document.getElementById("site-to-visit").value = "";
+
     });
-    console.log('finished setting commands');
+  }
+  else{
+    //user doesn't have pro and doesn't have access to custom commands
+    $('.pro_locked').fadeIn().delay(2000).fadeOut();
 
     //resetting input fields
     document.getElementById("custom-command").value = "";
     document.getElementById("site-to-visit").value = "";
-
-  });
+  }
 });
 
