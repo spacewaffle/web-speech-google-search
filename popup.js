50848a6409864973e45cfc818032d616edd9d58d
Jon Cheng <jon.syndicate@gmail.com>
2014-04-23 16:55:12 -0400

    added a go pro button

--- popup.js
+++ popup.js
@@ -214,4 +214,10 @@ option.addEventListener("click", function(){
   chrome.tabs.create({url: chrome.extension.getURL("options.html"),});
 });
 
+var go_pro = document.getElementById("go_pro");
+go_pro.addEventListener("click", function(){
+  //open the options page in a new tab in a different window
+  chrome.tabs.create({url: "https://chrome.google.com/webstore/category/extensions"});
+});
+
 })();
