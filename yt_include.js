10471903dfb357c029a1cda66f2329d9bd01f1b0
Jon Cheng <jon.syndicate@gmail.com>
2014-04-24 16:53:48 -0400

    added some comments

--- yt_include.js
+++ yt_include.js
@@ -1,3 +1,7 @@
+//this content script doesn't have access to the page's javscript since it's isolated by chrome
+//instead, we'll inject a different script into the page and bind event listeners to it to
+//communicate with the javascript of the chrome extension
+
 var s = document.createElement('script');
 // TODO: add "script.js" to web_accessible_resources in manifest.json
 s.src = chrome.extension.getURL('yt_setup.js');
