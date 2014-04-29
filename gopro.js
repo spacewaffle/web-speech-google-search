83303cacfa396cddca6b75ed714789ec39959e14
Jon Cheng <jon.syndicate@gmail.com>
2014-04-29 13:02:36 -0400

    redirect pro link in popup to app

--- gopro.js
+++ gopro.js
@@ -3,7 +3,7 @@
 var go_pro = document.getElementById("go_pro");
 go_pro.addEventListener("click", function(){
   //open the options page in a new tab in a different window
-  chrome.tabs.create({url: "https://chrome.google.com/webstore/category/extensions"});
+  chrome.tabs.create({url: "https://chrome.google.com/webstore/detail/nat-voice-commands/mnkjdemkpmiamhjdbbiihomainhabeob"});
 });
 
 })();
